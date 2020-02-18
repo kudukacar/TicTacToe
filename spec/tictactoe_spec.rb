bb55ba34e1390a61b24875066cff6d3926e9a37c
manju <manju.thomas083@gmail.com>
2020-02-18 12:38:33 -0500

    remove fakes

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -4,31 +4,11 @@ require "presenter"
 require "fake_standard_out"
 
 RSpec.describe "TicTacToe" do
-  class FakePresenter
-    def display_board
-      <<~BOARD
-          |  |
-        --+--+--
-          |  |
-        --+--+--
-          |  |
-      BOARD
-    end
-  end
-  class FakeDisplay
-    def initialize(stdout)
-      @stdout = stdout
-    end
-
-    def output(message)
-      @stdout.puts message
-    end
-  end
   describe "#run" do
     it "prints an empty 3 x 3 Tic-Tac-Toe board" do
       stdout = FakeStandardOut.new
-      display = FakeDisplay.new(stdout)
-      presenter = FakePresenter.new
+      display = Display.new(stdout)
+      presenter = Presenter.new
       tictactoe = TicTacToe.new(presenter, display)
       expected_board = <<~BOARD
           |  |
