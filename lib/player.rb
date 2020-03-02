9ada17ab16337f510907a2d0e0860eafbbf06c6f
Manju Thomas <kudukacar@hotmail.com>
2020-03-02 07:49:37 -0500

    add player prompt (#8)
    * add player prompt
    
    * refactor player prompt

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,7 @@ class Player
   attr_reader :display
 
   MESSAGES = {
+    player_prompt: "Go ",
     select_space: "Please select your move by entering the number (1 - 9, from top left to bottom right) of an empty space.",
     invalid_entry: "Invalid entry.",
     space_taken: "Selection taken and not available.",
@@ -12,11 +13,20 @@ class Player
   end
 
   def selection(board)
+    player_prompt(board)
     loop do
-      display.output(MESSAGES[:select_space])
+      selection_prompt
       selection = input_to_integer(display.input)
       return selection if selection_valid(selection) && selection_available(selection, board)
     end
+  end
+
+  def player_prompt(board)
+    display.output(MESSAGES[:player_prompt] + board.next_token)
+  end
+
+  def selection_prompt
+    display.output(MESSAGES[:select_space])
   end
 
   def selection_valid(position)
