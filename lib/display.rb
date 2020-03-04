f380c13cb0a2f04ca5b5207bd83ddb156faa06e1
manju <manju.thomas083@gmail.com>
2020-03-04 15:05:10 -0500

    add display prompt

--- lib/display.rb
+++ lib/display.rb
@@ -13,4 +13,29 @@ class Display
   def input
     stdin.gets.strip
   end
+
+  def prompt(message, parse: PassThroughParse.new, validation: PassThroughValidator.new)
+    loop do
+      output(message)
+      user_input = input
+      if error = validation.errors_for(user_input)
+        output(error)
+      else
+        return parse.call(user_input)
+      end
+    end
+  end
+
+  private
+
+  class PassThroughParse
+    def call(input)
+      input
+    end
+  end
+
+  class PassThroughValidator
+    def errors_for(input)
+    end
+  end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -44,4 +44,52 @@ RSpec.describe Display do
       expect(display.input).to eq("5")
     end
   end
+
+  describe "#prompt" do
+    class PlusTenAgeParser
+      def call(input)
+        input.to_i + 10
+      end
+    end
+
+    it "ask the user for input" do
+      display.prompt("How old are you?")
+
+      expect(stdout.messages).to include("How old are you?")
+    end
+
+    it "will return the users input" do
+      expect(display.prompt("Age?")).to eq("5")
+    end
+
+    context "with a parser" do
+      it "will parse the input" do
+        parsed_input = display.prompt("Age?", parse: PlusTenAgeParser.new)
+        
+        expect(parsed_input).to eq(15)
+      end
+    end
+
+    context "with a validator" do
+      class MustBeThree
+        def errors_for(input)
+          "#{input} is not Three" if input != "3"
+        end
+      end
+
+      it "will validate the input" do
+        validated_input = Display.new(stdout, FakeStandardIn.new(["-1", "-2", "3"]))
+                                 .prompt("Favorite Number?", validation: MustBeThree.new)
+
+        expect(validated_input).to include("3")
+      end
+
+      it "will display an error message if not valid" do
+        validated_input = Display.new(stdout, FakeStandardIn.new(["-1", "-2", "3"]))
+                                 .prompt("Favorite Number?", validation: MustBeThree.new)
+
+        expect(stdout.messages).to include("-1 is not Three", "-2 is not Three")
+      end
+    end
+  end
 end
