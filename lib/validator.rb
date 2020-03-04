c387bf9813bf5dfc9696afc86df1d72a21d14ce3
manju <manju.thomas083@gmail.com>
2020-03-04 15:39:54 -0500

    add available space validator

--- lib/validator.rb
+++ lib/validator.rb
@@ -7,3 +7,17 @@ class SelectionValidator
     OpenStruct.new(position: selection)
   end
 end
+
+class AvailableSpaceValidator
+  def initialize(availability)
+    @availability = availability
+  end
+
+  def errors_for(position)
+    return "Invalid" unless position.between?(1, 9)
+    return "Selection taken and not available" unless availability.is_available?(position)
+  end
+
+  private
+  attr_reader :availability
+end
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -39,3 +39,53 @@ RSpec.describe SelectionValidator do
     end
   end
 end
+
+RSpec.describe AvailableSpaceValidator do
+  class AvailabilityForTwo
+    def is_available?(selection)
+      return true if selection == 2
+    end
+  end
+
+  describe "#errors_for" do
+    context "when the selection is available" do
+      it "has no errors" do
+        errors = AvailableSpaceValidator.new(AvailabilityForTwo.new)
+          .errors_for(2)
+
+        expect(errors).to be_falsy
+      end
+    end
+
+    context "when the selection is not available" do
+      it "returns an error message" do
+        errors = AvailableSpaceValidator.new(AvailabilityForTwo.new)
+          .errors_for(1)
+
+        expect(errors).to include("Selection taken")
+      end
+    end
+
+    it "only validates spaces 1 through 9" do
+      validation = AvailableSpaceValidator.new(AvailabilityForTwo.new)
+
+      errors = (0..10).to_a.map do |input|
+        validation.errors_for(input)
+      end
+
+      expect(errors).to contain_exactly(
+        /Invalid/,
+        /Selection taken/,
+        nil,
+        /Selection taken/,
+        /Selection taken/,
+        /Selection taken/,
+        /Selection taken/,
+        /Selection taken/,
+        /Selection taken/,
+        /Selection taken/,
+        /Invalid/
+      )
+    end
+  end
+end
